e0cba559101febafca6581abd371b51da7c1e190
heeyeoul kim <kheeyeoul@gmail.com>
2023-10-24 21:02:59 +0900

    [update] JWT 인증/인가 필터 추가

---
+++ src/main/java/com/example/prac/common/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.prac.common.security;
+
+import com.example.prac.user.entity.User;
+import com.example.prac.user.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Not found " + email));
+
+        return new UserDetailsImpl(user);
+    }
+}
--- src/main/java/com/example/prac/user/service/UserService.java
+++ src/main/java/com/example/prac/user/service/UserService.java
@@ -5,6 +5,8 @@ import com.example.prac.user.entity.User;
 import com.example.prac.user.entity.UserRoleEnum;
 import com.example.prac.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,25 +15,37 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    private static final String adminToken = "ADMIN";
+    @Value("${admin.token}")
+    private String adminToken;
 
     @Transactional
     public void signup(SignupRequestDto requestDto) {
+        String password = passwordEncoder.encode(requestDto.getPassword());
+
+        checkDuplicateEmail(requestDto.getEmail());
+
         UserRoleEnum role = UserRoleEnum.USER;
         if (requestDto.getAdminToken() != null && requestDto.getAdminToken().equals(adminToken)) {
             role = UserRoleEnum.ADMIN;
         }
 
-        userRepository.save(requestDto.toEntity(requestDto.getPassword(), role));
+        userRepository.save(requestDto.toEntity(password, role));
     }
 
     @Transactional
     public User getUserInfo(long userId) {
-        return findUser(userId);
+        return findUserById(userId);
     }
 
-    private User findUser(long userId) {
+    private void checkDuplicateEmail(String email) {
+        if (userRepository.existsByEmail(email)) {
+            throw new IllegalArgumentException("이미 가입된 이메일입니다.");
+        }
+    }
+
+    private User findUserById(long userId) {
         return userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
     }
 }
